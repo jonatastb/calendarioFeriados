78585f5f5f2af9adf39209dd0601ca22727f0a9c
JonatasTB <101901086+jonatastb@users.noreply.github.com>
2023-07-22 14:27:04 -0300

    :recycle: refactor: Remove Lines

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import retornaProximosFeriados from './retornaFeriados.js';
 import imprimeFeriados from './imprimeFeriados.js'
-console.log('\nOs próximos feriados serão:\n')
+console.log('\nOs próximos feriados, lista completa:\n')
 let numeroFeriados = 3;
 
 let proximosFeriados = retornaProximosFeriados(numeroFeriados);
